21e5432c5491f2cf68266ea1a32b37cc0dad2efd
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-05-04 16:34:54 -0500

    filter 'ignored' fields

--- json_generator.py
+++ json_generator.py
@@ -1,22 +1,38 @@
 FILE_PATH = 'json_text.txt'
+NEW_FILE_NAME = 'formatted_json.json'
 
-with open(FILE_PATH) as f:
+with open(FILE_PATH) as f1:
+    with open(NEW_FILE_NAME, 'w') as f2:
 
-    # begin reading lines
-    # line = f.readline()
-    curr = 0
+        # format database
+        f2.write('{\n')
+        f2.write('\t"businesses": [\n')
+        f2.write('\t\t{\n')
 
-    while curr < 3:
+        # rewrite title
+        line = f1.readline().split(':')[0] + ","
+        f2.write(f'\t\t\t"name": {line}\n')
 
-        # read line
-        line = f.readline()
-        print(f"Line {curr}: {line.strip()}")
+        # begin iterating
+        curr = 1
 
-        # format line
-        line = line.replace('“', '"')
-        line = line.replace('”', '"')
+        while curr < 13:
 
-        # write line
-        print(f"Line {curr}: {line.strip()}\n")
+            # read line
+            line = f1.readline()
 
-        curr += 1
+            # format line
+            line = line.replace('“', '"')
+            line = line.replace('”', '"')
+            line = line.replace('’', "'")
+            if ('Policies' in line) or ('Delivery' in line) or ('Reservations' in line) or \
+                ('Price Range' in line) or ('Discounts' in line):
+                continue
+
+            # write line
+            line = '\t\t' + line
+            f2.write(line)
+
+            curr += 1
+
+        f2.write('\t]\n}')
